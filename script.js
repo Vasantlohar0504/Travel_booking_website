c1a30c9637dc0a1e84acbbbed966fb4f1957b0ad
Vasantlohar0504 <vasantlohar0504@gmail.com>
2024-11-18 13:44:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    
 });
-// Handle the form submission
 document.getElementById('bookingDetailsForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent the form from submitting the traditional way
+    e.preventDefault(); 
 
     // Collect form data
     const formData = new FormData(this);
@@ -14,7 +11,6 @@ document.getElementById('bookingDetailsForm').addEventListener('submit', functio
     const dates = formData.get('dates');
     const specialRequest = formData.get('specialRequest');
 
-    // For demonstration, we'll just log the data to the console
     console.log('Booking Details:');
     console.log(`Destination: ${destination}`);
     console.log(`Full Name: ${fullName}`);
@@ -25,9 +21,9 @@ document.getElementById('bookingDetailsForm').addEventListener('submit', functio
     // Display success message
     document.getElementById('booking-message').style.display = 'block';
 
-    // Optionally, you can redirect the user to a confirmation page or do other actions
-    // Example: Redirect to a confirmation page after 3 seconds
+    // Optionally, you can redirect the user to a confirmation page 
+
     setTimeout(function() {
-        window.location.href = 'confirmation.html'; // Redirect to a confirmation page
-    }, 3000);
+        window.location.href = 'confirmation.html'; 
+    }, 1000);
 });
